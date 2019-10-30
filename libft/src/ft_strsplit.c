343fc100fa47a8fa404b842fddadfa2b55f7affb
nygymankussainov <nygmankusainov23@gmail.com>
2019-10-30 14:44:15 +0300

    added libft

---
+++ libft/src/ft_strsplit.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vhazelnu <vhazelnu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/23 11:35:37 by vhazelnu          #+#    #+#             */
+/*   Updated: 2019/08/26 12:17:46 by vhazelnu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int		ft_nbw(const char *str, char c)
+{
+	int word;
+
+	word = 0;
+	if (*str != c && *str)
+	{
+		str++;
+		word++;
+	}
+	while (*str)
+	{
+		while (*str == c)
+		{
+			str++;
+			if (*str != c && *str)
+				word++;
+		}
+		str++;
+	}
+	return (word);
+}
+
+static int		ft_ln(const char *str, char c)
+{
+	int count;
+
+	count = 0;
+	while (*str != c && *str)
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+char			**ft_strsplit(char const *s, char c)
+{
+	int		j;
+	int		i;
+	char	**spt;
+
+	j = 0;
+	i = 0;
+	if (!s || (!(spt = (char **)malloc(sizeof(char *) * (ft_nbw(s, c) + 1)))))
+		return (NULL);
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s != c && *s)
+		{
+			if (!(spt[j] = (char *)malloc(sizeof(char) * (ft_ln(s, c) + 1))))
+				return (NULL);
+			while (*s && *s != c)
+				spt[j][i++] = (char)*s++;
+			spt[j][i] = '\0';
+			j++;
+			i = 0;
+		}
+	}
+	spt[j] = NULL;
+	return (spt);
+}
