bd7d55155910583fa695ead084455f942b0c117a
nygymankussainov <nygmankusainov23@gmail.com>
2019-11-01 22:29:34 +0300

    debugging validation of .name and .comment

--- Assembler/asm.h
+++ Assembler/asm.h
@@ -6,7 +6,7 @@
 /*   By: vhazelnu <vhazelnu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 14:36:26 by vhazelnu          #+#    #+#             */
-/*   Updated: 2019/10/30 18:10:05 by vhazelnu         ###   ########.fr       */
+/*   Updated: 2019/11/01 22:23:44 by vhazelnu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,20 @@
 # include "op.h"
 # include "ft_printf.h"
 
-int		validation(char *file);
-int		validate_file(char *file);
+typedef	struct 	s_major
+{
+	int			fd;
+	int			row;
+	int			col;
+}				t_major;
+
+typedef enum	e_err_type
+{
+	Syntax = 1,
+	Lexical
+}				t_err_type;
+
+int				validation(char *file, t_major *major);
+int				validate_file(char *file);
 
 #endif
--- Assembler/src/validation.c
+++ Assembler/src/validation.c
@@ -6,76 +6,88 @@
 /*   By: vhazelnu <vhazelnu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 18:07:38 by vhazelnu          #+#    #+#             */
-/*   Updated: 2019/11/01 17:07:25 by vhazelnu         ###   ########.fr       */
+/*   Updated: 2019/11/01 22:28:28 by vhazelnu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-int		print_error(char *line, int type, int i, int j)
+int		print_error(char **line, int type, char *message, t_major *major)
 {
-	if (type == 1)
+	if (type == Syntax)
+		putstrerr("Syntax error. ");
+	else if (type == Lexical)
+		putstrerr("Lexical error. ");
+	putstrerr(message);
+	ft_printf("[%d:%d].\n", major->row, major->col);
+	ft_strdel(line);
+	exit(type);
+}
+
+void	find_first_quote(char **line, t_major *major)
+{
+	while (line && *line && (*line)[major->col] && (*line)[major->col] != '"')
 	{
-		putstrerr("Syntax error. Missing .name or .comment on line ");
-		ft_printf("%d.\n", i);
+		if (ft_isascii((*line)[major->col]) && !iswhitesp((*line)[major->col]))
+			print_error(line, Lexical, "Missing quote at ", major);
+		major->col++;
 	}
-	else if (type == 2)
+	if ((*line)[major->col] == '\0')
+		print_error(line, Lexical, "Missing quote at ", major);
+	major->col++;
+}
+
+int		find_second_quote(char **line, t_major *major)
+{
+	while (line && *line && (*line)[major->col] && (*line)[major->col] != '"')
+		major->col++;
+	if (line && *line && (*line)[major->col] == '"')
 	{
-		putstrerr("Lexical error. Missing quote at ");
-		ft_printf("[%d:%d].\n", i, j);
+		if ((*line)[major->col + 1] == '\0')
+			return (1);
+		else
+			print_error(line, Lexical, "Invalid symbol after quote at ", major);
 	}
-	else if (type == 3)
-	{
-		putstrerr("Lexical error. Extra quote at ");
-		ft_printf("[%d:%d].\n", i, j);
-	}
-	ft_strdel(&line);
 	return (0);
 }
 
-int		check_format(char *line, int i)
+int		check_quotes(int fd, char **line, t_major *major)
 {
-	int		len;
-	int		j;
-
-	len = ft_strlen(line);
-	if (line[len - 1] != '"')
-		return (print_error(line, 2, i, len));
-	j = !ft_strncmp(line, ".name", 5) ? 5 : 8;
-	if (ft_isalnum(line[j]))
-		return (print_error(line, 2, i, j));
-	while (line[j] && line[j] != '"')
-		j++;
-	if ((ft_isascii(line[j - 1]) && !iswhitesp(line[j - 1])) ||
-		len - 1 - j++ == 0)
+	find_first_quote(line, major);
+	if (!find_second_quote(line, major))
 	{
-		j = !ft_strncmp(line, ".name", 5) ? 5 : 8;
-		while (!ft_isalnum(line[j]))
-			j++;
-		return (print_error(line, 2, i, j));
+		ft_strdel(line);
+		while (get_next_line(fd, line))
+		{
+			major->row++;
+			major->col = 0;
+			if (find_second_quote(line, major))
+				return (1);
+			ft_strdel(line);
+		}
+		print_error(line, Lexical, "Missing quote at ", major);
 	}
-	while (line[j] && line[j] != '"')
-		j++;
-	if (len - 1 - j != 0)
-		return (print_error(line, 3, i, j));
 	return (1);
 }
 
-int		validate_name_and_comment(int fd, char *line)
+int		validate_name_and_comment(int fd, char *line, t_major *major)
 {
 	int		ret;
-	int		i;
 
 	ret = 0;
-	i = 1;
+	major->row = 1;
 	while (get_next_line(fd, &line))
 	{
 		if (line && line[0] != '#' && line[0] != '.' && ret < 2)
-			return (print_error(line, 1, i - 1, 0));
+		{
+			major->row--;
+			print_error(&line, Syntax, "Missing .name or .comment at ", major);
+		}
 		if (line && (!ft_strncmp(line, ".name", 5) ||
 			!ft_strncmp(line, ".comment", 8)))
 		{
-			if (!check_format(line, i))
+			major->col = !ft_strncmp(line, ".name", 5) ? 5 : 8;
+			if (!check_quotes(fd, &line, major))
 				return (0);
 			ret++;
 		}
@@ -85,19 +97,19 @@ int		validate_name_and_comment(int fd, char *line)
 			return (1);
 		}
 		ft_strdel(&line);
-		i++;
+		major->row++;
 	}
 	return (0);
 }
 
-int		validation(char *file)
+int		validation(char *file, t_major *major)
 {
 	int		fd;
 	char	*line;
 
 	line = NULL;
 	if (!(fd = validate_file(file)) ||
-		!validate_name_and_comment(fd, line))
+		!validate_name_and_comment(fd, line, major))
 	{
 		if (fd > 0)
 			while (get_next_line(fd, &line))
