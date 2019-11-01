f870b9963716f55d411104e600a6a7c59e5844c3
nygymankussainov <nygmankusainov23@gmail.com>
2019-11-01 16:16:59 +0300

    finished validation of .name and .comment

--- Assembler/src/validation.c
+++ Assembler/src/validation.c
@@ -6,42 +6,60 @@
 /*   By: vhazelnu <vhazelnu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 18:07:38 by vhazelnu          #+#    #+#             */
-/*   Updated: 2019/10/30 22:14:00 by vhazelnu         ###   ########.fr       */
+/*   Updated: 2019/11/01 16:14:46 by vhazelnu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-int		print_error(char *line, int i)
+int		print_error(char *line, int type, int i, int j)
 {
-	if (i == 1)
+	if (type == 1)
 	{
-		putstrerr("Syntax error. ");
-		putstrerr("Missing .name or .comment at the top of a file.\n");
-		ft_strdel(&line);
+		putstrerr("Syntax error. Missing .name or .comment on line ");
+		ft_printf("%d.\n", i);
 	}
+	else if (type == 2)
+	{
+		putstrerr("Lexical error. Missing quote at ");
+		ft_printf("[%d:%d].\n", i, j);
+	}
+	else if (type == 3)
+	{
+		putstrerr("Lexical error. Extra quote at ");
+		ft_printf("[%d:%d].\n", i, j);
+	}
+	ft_strdel(&line);
 	return (0);
 }
 
 int		check_format(char *line, int i)
 {
-	char	*begin;
+	int		len;
+	int		j;
 
-	begin = line;
-	while (*line && *line != '"')
-		line++;
-	while (line && *line)
+	len = ft_strlen(line);
+	if (line[len - 1] != '"')
+		return (print_error(line, 2, i, len));
+	j = !ft_strncmp(line, ".name", 5) ? 5 : 8;
+	if (ft_isalnum(line[j]))
+		return (print_error(line, 2, i, j));
+	while (line[j] && line[j] != '"')
+		j++;
+	if (len - 1 - j++ == 0)
 	{
-		line++;
-		if (*line == '"' && *(line + 1) == '\0')
-			return (1);
-		else if (*line == '"' && *(line + 1) != '\0')
-			break ;
+		j = 0;
+		while (!iswhitesp(line[j]))
+			j++;
+		while (!ft_isalnum(line[j]))
+			j++;
+		return (print_error(line, 2, i, j));
 	}
-	putstrerr("Lexical error. Must be covered in one pait of quotes on line ");
-	ft_printf("%d.\n", i);
-	ft_strdel(&begin);
-	return (0);
+	while (line[j] && line[j] != '"')
+		j++;
+	if (len - 1 - j != 0)
+		return (print_error(line, 3, i, j));
+	return (1);
 }
 
 int		validate_name_and_comment(int fd, char *line)
@@ -54,7 +72,7 @@ int		validate_name_and_comment(int fd, char *line)
 	while (get_next_line(fd, &line))
 	{
 		if (line && line[0] != '#' && line[0] != '.' && ret < 2)
-			return (print_error(line, 1));
+			return (print_error(line, 1, i - 1, 0));
 		if (line && (!ft_strncmp(line, ".name", 5) ||
 			!ft_strncmp(line, ".comment", 8)))
 		{
@@ -76,9 +94,16 @@ int		validate_name_and_comment(int fd, char *line)
 int		validation(char *file)
 {
 	int		fd;
+	char	*line;
 
+	line = NULL;
 	if (!(fd = validate_file(file)) ||
-		!validate_name_and_comment(fd, NULL))
+		!validate_name_and_comment(fd, line))
+	{
+		if (fd > 0)
+			while (get_next_line(fd, &line))
+				ft_strdel(&line);
 		return (0);
+	}
 	return (fd);
 }
