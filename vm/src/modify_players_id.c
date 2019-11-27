df15b54307a78ed3e4764327f2c7924e17f06948
Nygyman Kussainov <nygmankusainov23@gmail.com>
2019-11-27 14:51:06 +0300

    added swap_players func

--- vm/src/modify_players_id.c
+++ vm/src/modify_players_id.c
@@ -6,11 +6,21 @@
 /*   By: vhazelnu <vhazelnu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 18:29:45 by vhazelnu          #+#    #+#             */
-/*   Updated: 2019/11/27 13:54:17 by vhazelnu         ###   ########.fr       */
+/*   Updated: 2019/11/27 14:48:17 by vhazelnu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
+
+void	swap_players(t_player *a, t_player *b)
+{
+	t_player c;
+
+	ft_swap(&a->id, &b->id);
+	c = *a;
+	*a = *b;
+	*b = c;
+}
 
 void	sort_players(t_player *player, int nb, int reserve)
 {
@@ -26,11 +36,11 @@ void	sort_players(t_player *player, int nb, int reserve)
 				if (i + 1 < nb && player[i + 1].id != reserve)
 				{
 					if (i + 1 < nb && player[i].id > player[i + 1].id)
-						ft_swap(&player[i].id, &player[i + 1].id);
+						swap_players(&player[i], &player[i + 1]);
 				}
 				else if (i + 2 < nb && player[i].id > player[i + 2].id)
 				{
-					ft_swap(&player[i].id, &player[i + 2].id);
+					swap_players(&player[i], &player[i + 2]);
 					++i;
 				}
 			}
@@ -73,7 +83,7 @@ void	modify_players_id(t_player *player, char **argv, int argc, int nb)
 			if (reserve <= 0 || reserve > nb)
 				return ;
 			tmp = find_player(player, reserve, nb);
-			ft_swap(&player[j].id, &player[tmp].id);
+			swap_players(&player[j], &player[tmp]);
 			sort_players(player, nb, reserve);
 		}
 		i++;
